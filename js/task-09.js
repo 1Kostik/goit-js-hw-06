9283bf2139700dcf0932dfde2b8d10b9896445b3
1Kostik <117483719+1Kostik@users.noreply.github.com>
2023-03-17 11:21:56 +0200

    up

--- js/task-09.js
+++ js/task-09.js
@@ -1,17 +1,22 @@
 const button = document.querySelector(".change-color");
+
 button.addEventListener("click", onButtonClick);
+
 const bgColor = document.querySelector(".color");
+
 const body = document.body;
 
 function onButtonClick() {
   function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215)
+
       .toString(16)
+
       .padStart(6, 0)}`;
   }
   body.style.backgroundColor = getRandomHexColor();
-  console.log(body.style.backgroundColor)
+
+  console.log(body.style.backgroundColor);
 
   bgColor.textContent = `${body.style.backgroundColor}`;
-  
 }
